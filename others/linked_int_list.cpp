c7b077765fb4b470b89fc98460bea17c300bf1ff
hxnn3sgit <seihannes@gmx.de>
2025-05-07 21:59:30 +0200

    finished linked_int_list and started working on storing meeting names in linked list

---
+++ others/linked_int_list.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+class int_list {
+private:
+    struct node {
+        int data;
+        struct node *next;
+        node (int d) : data(d), next(nullptr) {}
+    };
+    node *head;
+public:
+    int_list() : head(nullptr) {}
+    ~int_list();
+    int len();
+    bool is_empty();
+    void append(int payload);
+    void prepend(int payload);
+    friend ostream &operator<<(ostream& os, const int_list &my_list);
+};
+
+int_list::~int_list() {
+    //delete the list
+}
+
+int int_list::len() {
+    // go through list and count every node
+    // return len
+    int count = 0;
+    node *run = head;
+    while (run) {
+        count++;
+        run = run->next;
+    }
+    return count;
+}
+
+bool int_list::is_empty() {
+    return head == nullptr; // if head == nullptr list is empty and true is returned, otherwise false
+}
+
+void int_list::append(int payload) {
+    if (head == nullptr) { // empty list
+        node *new_node = new node(payload); //new_node->next already nullptr
+        head = new_node;
+    } else {
+		node *new_node = new node(payload);
+		node *run = head;
+
+		while (run->next != nullptr) {
+			run = run->next;
+		}
+		run->next = new_node;
+	}
+}
+
+ostream& operator<<(ostream& os, const int_list &my_list) {
+	int_list::node *run = my_list.head;
+    while (run) {
+        os << "[" << run->data << "]-";
+        run = run->next;
+    }
+    os << "|";
+    
+    return os;
+}
+
+int main() {
+    
+    int_list my_list;
+    my_list.append(1);
+    my_list.append(2);
+    my_list.append(3);
+    
+    cout << my_list << endl;
+    
+    
+
+    return 0;
+}
