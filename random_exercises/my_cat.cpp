d897a75267061032ae7792383b67922b9f78dda4
hxnn3sgit <seihannes@gmx.de>
2025-06-14 12:24:55 +0200

    my_cat v1 finished

--- random_exercises/my_cat.cpp
+++ random_exercises/my_cat.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <exception>
+#include <string>
 
 using std::cout, std::endl, std::string;
 
-void concatenate(std::istream &in, const std::vector<string> &args, bool line_flag = false) {
-	
-}
+class file_not_opened : public std::exception {
+public:
+	virtual const char *what() const noexcept override {
+		return "file could not be opened";
+	}			
+};
 
-void concatenate(std::istream &in) {
-	string line;
-	while (std::getline(in, line))
-		cout << line << endl;
-}
-
-void concatenate(std::istream &in, bool line_flag) {
+void concatenate(std::istream &in, bool line_flag = false) {
 	string line;
 	if (line_flag) {
 		int line_nr = 1;
 		while (std::getline(in, line)) {
-			cout << "\t " << line_nr++ << "  "  << line << endl;
+			cout << "  " << line_nr++ << "\t" << "  "  << line << endl;
 		}
 	} else {
 		while(std::getline(in, line)) 
@@ -27,24 +26,55 @@ void concatenate(std::istream &in, bool line_flag) {
 	}	
 }
 
-bool check_for_line_flag(const std::vector<string> &args) {
-	for (const auto &arg : args) {
-		if (arg == "-n")
-			return true;
+bool check_for_line_flag(std::vector<string> &args) {
+	bool line_flag = false;
+	for (int i = 0; i < args.size(); ++i) {
+		if (args.at(i) == "-n"){
+			args.erase(args.begin() + i);
+			line_flag = true;
+		}
 	}
-	return false;
+	return line_flag;
 }
 
 int main(int argc, char **argv) {
-	if (argc == 1) {
-		concatenate(std::cin, true);	
-	} else {
+	if (argc == 1) { // reading over std::cin with no line-flag
+		concatenate(std::cin, false);	
+	} else if (argc == 2) { // reading over std::cin with line-flag
+		string argv_2 = argv[1];
+ 		if (argv_2 == "-n")
+			concatenate(std::cin, true);
+		else { // read from file
+			std::ifstream my_file;
+			my_file.open(argv_2);
+			if (!my_file.is_open()) {
+				throw file_not_opened();
+			} else {
+				concatenate(my_file);
+			}
+		}
+	} else if (argc > 1) { // reading from file
 		std::vector<string> args;
-		for (int i = 0; i < argc; ++i)
+		for (int i = 1; i < argc; ++i)
 			args.push_back(argv[i]);
 		
 		bool line_flag = check_for_line_flag(args);
-
+		
+		for(const auto &arg : args) {
+			try {
+				std::ifstream my_file;
+				my_file.open(arg);
+				if (my_file.is_open()) {
+					concatenate(my_file, line_flag);
+				} else {
+					throw file_not_opened();
+				}
+			} catch (file_not_opened &e) {
+				std::cerr << "error while opening file: " << e.what() << endl;
+			} catch (std::exception &e) {
+				std::cerr << "exception occured: " << e.what() << endl;
+			}
+		}
 	}
 	
 	return 0;
